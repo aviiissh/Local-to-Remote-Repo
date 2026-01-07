b5bc933bf8a79d96637437e8220a21ac38418061
aviiissh <avish2314@gmail.com>
2026-01-07 15:57:30 +0530

    data is transformed

---
+++ data_transform.py
@@ -0,0 +1,6 @@
+#write pyhton function for data transformation
+def data_transformation(df):
+    #logic for transforming data
+    transformed_df = df.copy()  # Example transformation logic
+    # Add your transformation logic here
+    return transformed_df
